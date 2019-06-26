e98f3faae716f3f20cc8c2fe8d5a895df6bbc7fa
Alexander <1alexvash@gmail.com>
2019-06-26 11:33:00 +0300

    Updates

---
+++ precache-manifest.31d77a734b377a4ae766a76b39e342bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f503182f1d0a629da716711e54139d14",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "f6000b00e505031663f4",
+    "url": "/Weather/static/css/main.0ad8bbd2.chunk.css"
+  },
+  {
+    "revision": "b2456e3f46a31c405840",
+    "url": "/Weather/static/js/2.aa5c6f99.chunk.js"
+  },
+  {
+    "revision": "f6000b00e505031663f4",
+    "url": "/Weather/static/js/main.db8c8abb.chunk.js"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/Weather/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/Weather/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/Weather/static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "f3ce0ef136eaaf2b1db22cf4069cb5dc",
+    "url": "/Weather/static/media/owfont-regular.f3ce0ef1.svg"
+  }
+]);
